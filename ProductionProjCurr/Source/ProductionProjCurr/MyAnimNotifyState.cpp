7b606d6d3da426c3dccfa29110bda969d2808451
oliveeah <olliebennettx@gmail.com>
2025-09-28 11:41:02 +0000

    got rid of anim notify as i meant to add a nimnotifystate (why tickNotify wasnt working)

---
+++ ProductionProjCurr/Source/ProductionProjCurr/MyAnimNotifyState.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyAnimNotifyState.h"
+
+void UMyAnimNotifyState::NotifyTick(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, float FrameDeltaTime, const FAnimNotifyEventReference& EventReference)
+{
+}
