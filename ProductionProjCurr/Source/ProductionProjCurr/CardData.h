95da2d6fbc026e0aedd7f148b5328dcea7601f60
oliveeah <olliebennettx@gmail.com>
2025-09-29 15:00:14 +0000

    added some template card designs and created the widget to work on. started work on a primary data asset which will be inherited from (polymorphism)

---
+++ ProductionProjCurr/Source/ProductionProjCurr/CardData.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CardData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRODUCTIONPROJCURR_API UCardData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
