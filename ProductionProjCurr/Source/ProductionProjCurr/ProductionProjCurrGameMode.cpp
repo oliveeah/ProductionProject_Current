0f7693debbda634efcc827179cbde5c9b525edbc
oliveeah <olliebennettx@gmail.com>
2025-10-21 15:39:09 +0000

    thinking

--- ProductionProjCurr/Source/ProductionProjCurr/ProductionProjCurrGameMode.cpp
+++ ProductionProjCurr/Source/ProductionProjCurr/ProductionProjCurrGameMode.cpp
@@ -21,7 +21,7 @@ void AProductionProjCurrGameMode::BeginPlay()
 
 AProductionProjCurrGameMode::AProductionProjCurrGameMode()
 {
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/MyStuff/Blueprints/Player/BP_Mushroom.BP_Mushroom"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/MyStuff/Blueprints/Player/BP_Bunnt.BP_Bunnt"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	//static ConstructorHelpers::FClassFinder<UUserWidget> PlayerWidgetClassFinder(TEXT("/Game/MyStuff/Blueprints/UI/Player_Widget.Player_Widget"));
