a5f314e0d06d64815fb0c6650271009f21ec445b
oliveeah <olliebennettx@gmail.com>
2025-10-22 11:44:06 +0000

    stopping to stroke the dog

--- ProductionProjCurr/Source/ProductionProjCurr/ProductionProjCurrGameMode.cpp
+++ ProductionProjCurr/Source/ProductionProjCurr/ProductionProjCurrGameMode.cpp
@@ -21,7 +21,7 @@ void AProductionProjCurrGameMode::BeginPlay()
 
 AProductionProjCurrGameMode::AProductionProjCurrGameMode()
 {
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/MyStuff/Blueprints/Player/BP_Bunnt.BP_Bunnt"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/MyStuff/Blueprints/Player/BP_Mushroom.BP_Mushroom"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	//static ConstructorHelpers::FClassFinder<UUserWidget> PlayerWidgetClassFinder(TEXT("/Game/MyStuff/Blueprints/UI/Player_Widget.Player_Widget"));
